a692f2195c7b84854ac53df3259f9db3e6bed1b3
Patrick Antoine <patrick.antoine@global66.com>
2021-06-01 02:25:34 -0400

    Configuración ESLINT Creación .browserslistrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,36 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    "@nuxtjs",
+    "eslint:recommended",
+    "plugin:nuxt/recommended"
   ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+  plugins: [],
+  rules: {
+    semi: [2, "always"],
+    quotes: [2, "double", { avoidEscape: true }],
+    "vue/html-closing-bracket-newline": [2, {
+      singleline: "never",
+      multiline: "never"
+    }],
+    "vue/no-static-inline-styles": [2],
+    "vue/no-unused-properties": [2, {
+      groups: ["props", "data", "computed", "methods"],
+      deepData: true
+    }],
+    "vue/static-class-names-order": [2],
+    "vue/max-attributes-per-line": [2, {
+      singleline: {
+        max: 1,
+        allowFirstLine: true
+      },
+      multiline: {
+        max: 1,
+        allowFirstLine: true
+      }
+    }]
+  }
+};
